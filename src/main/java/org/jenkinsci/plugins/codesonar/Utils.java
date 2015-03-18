9e8df01ed32bda7227082f70e687601ab78b2927
Andrius Ordojan <andrius.or@gmail.com>
2015-03-18 17:06:27 +0100

    clean up

--- src/main/java/org/jenkinsci/plugins/codesonar/Utils.java
+++ src/main/java/org/jenkinsci/plugins/codesonar/Utils.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.jenkinsci.plugins.codesonar;
+ package org.jenkinsci.plugins.codesonar;
 
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
