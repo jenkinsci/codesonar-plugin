ebf6c8bd59abef330e8f6b6d67a0db2af70ac923
Andrius Ordojan <andrius.or@gmail.com>
2015-03-18 17:06:16 +0100

    added a model for an alert

---
+++ src/main/java/org/jenkinsci/plugins/codesonar/models/Alert.java
@@ -0,0 +1,83 @@
+package org.jenkinsci.plugins.codesonar.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Andrius
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Alert implements Serializable {
+
+    @XmlAttribute
+    private String url;
+    @XmlAttribute
+    private String message;
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.url);
+        hash = 29 * hash + Objects.hashCode(this.message);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Alert other = (Alert) obj;
+        if (!Objects.equals(this.url, other.url)) {
+            return false;
+        }
+        if (!Objects.equals(this.message, other.message)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Alert{" + "url=" + url + ", message=" + message + '}';
+    }
+
+}
