a913aca5be48a154768d88fb148edc769df69c39
Andrius Ordojan <andrius.or@gmail.com>
2015-03-20 16:51:15 +0100

    Created a model for metrics

---
+++ src/main/java/org/jenkinsci/plugins/codesonar/models/metrics/Metric.java
@@ -0,0 +1,123 @@
+package org.jenkinsci.plugins.codesonar.models.metrics;
+
+import java.util.Objects;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Andrius
+ */
+@XmlRootElement(name = "metric")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Metric {
+    @XmlAttribute
+    private String url;
+    private String name;
+    private String description;
+    private String granularity;
+    private String file;
+    private String procedure;
+    private String value;
+
+    public String getProcedure() {
+        return procedure;
+    }
+
+    public void setProcedure(String procedure) {
+        this.procedure = procedure;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGranularity() {
+        return granularity;
+    }
+
+    public void setGranularity(String granularity) {
+        this.granularity = granularity;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.url);
+        hash = 53 * hash + Objects.hashCode(this.name);
+        hash = 53 * hash + Objects.hashCode(this.description);
+        hash = 53 * hash + Objects.hashCode(this.granularity);
+        hash = 53 * hash + Objects.hashCode(this.value);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Metric other = (Metric) obj;
+        if (!Objects.equals(this.url, other.url)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.granularity, other.granularity)) {
+            return false;
+        }
+        if (!Objects.equals(this.value, other.value)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Metric{" + "url=" + url + ", name=" + name + ", description=" + description + ", granularity=" + granularity + ", file=" + file + ", procedure=" + procedure + ", value=" + value + '}';
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/codesonar/models/metrics/Metrics.java
@@ -0,0 +1,60 @@
+package org.jenkinsci.plugins.codesonar.models.metrics;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Andrius
+ */
+@XmlRootElement(name = "metrics")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Metrics implements Serializable {
+    @XmlElement(name = "metric")
+    private List<Metric> metrics;
+
+    public List<Metric> getMetrics() {
+        if (metrics == null) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return metrics;
+    }
+
+    public void setMetrics(List<Metric> metrics) {
+        this.metrics = metrics;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.metrics);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Metrics other = (Metrics) obj;
+        if (!Objects.equals(this.metrics, other.metrics)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Metrics{" + "metrics=" + metrics + '}';
+    }
+}
