6e76e67dfacba6668b6f05239500c526a7dc2e10
Andrius Ordojan <andrius.or@gmail.com>
2015-03-26 18:21:15 +0100

    implemented condition to limit the yellow alerts on the codesonar hub

---
+++ src/main/java/org/jenkinsci/plugins/codesonar/conditions/YellowAlertLimitCondition.java
@@ -0,0 +1,81 @@
+package org.jenkinsci.plugins.codesonar.conditions;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import java.util.List;
+import org.jenkinsci.plugins.codesonar.CodeSonarBuildAction;
+import org.jenkinsci.plugins.codesonar.models.Alert;
+import org.jenkinsci.plugins.codesonar.models.Analysis;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+/**
+ *
+ * @author Andrius
+ */
+public class YellowAlertLimitCondition extends Condition {
+    private static final String NAME = "Yellow alert limit";
+   
+    private int alertLimit = 0;
+    private String warrantedResult = Result.UNSTABLE.toString();
+
+    @DataBoundConstructor
+    public YellowAlertLimitCondition(int alertLimit) {
+        this.alertLimit = alertLimit;
+    }
+    
+    @Override
+    public Result validate(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+        CodeSonarBuildAction buildAction = build.getAction(CodeSonarBuildAction.class);
+        if (buildAction == null) {
+            return Result.SUCCESS;
+        }
+
+        Analysis analysis = buildAction.getAnalysis();
+        if (analysis == null) {
+            return Result.SUCCESS;
+        }
+        
+        List<Alert> yellowAlerts = analysis.getYellowAlerts();
+        if (yellowAlerts.size() > alertLimit) {
+            Result result = Result.fromString(warrantedResult);
+            return result;
+        }
+
+        return Result.SUCCESS;
+    }
+    
+    public int getAlertLimit() {
+        return alertLimit;
+    }
+
+    @DataBoundSetter
+    public void setAlertLimit(int alertLimit) {
+        this.alertLimit = alertLimit;
+    }
+   
+    public String getWarrantedResult() {
+        return warrantedResult;
+    }
+
+    @DataBoundSetter
+    public void setWarrantedResult(String warrantedResult) {
+        this.warrantedResult = warrantedResult;
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends ConditionDescriptor<YellowAlertLimitCondition> {
+
+        public DescriptorImpl() {
+            load();
+        }
+
+        @Override
+        public String getDisplayName() {
+            return NAME;
+        }
+    }
+}
