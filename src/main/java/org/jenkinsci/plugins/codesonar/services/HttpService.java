3f49f2961c02b546ed3336c4040288bb5711e293
Andrius Ordojan <andrius.or@gmail.com>
2016-01-18 15:11:49 +0100

    add the use of cookie store

--- src/main/java/org/jenkinsci/plugins/codesonar/services/HttpService.java
+++ src/main/java/org/jenkinsci/plugins/codesonar/services/HttpService.java
@@ -3,19 +3,31 @@ package org.jenkinsci.plugins.codesonar.services;
 import hudson.AbortException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
 
 /**
  *
  * @author Andrius
  */
 public class HttpService implements Serializable {
+    CookieStore httpCookieStore;
+    Executor executor;
+    
+    public HttpService() {
+        httpCookieStore = new BasicCookieStore();
+        executor = Executor.newInstance();
+    }
     
     private static final Logger logger = Logger.getLogger(HttpService.class.getName());
     
@@ -25,10 +37,12 @@ public class HttpService implements Serializable {
         System.out.println("------------url----------------");
         System.out.println(url);
         
-        logger.fine(String.format("Request sent to %s", url));        
+        logger.fine(String.format("Request sent to %s", url));       
+
         String output;
         try {
-            Response response = Request.Get(url).execute();
+            Response response = executor.cookieStore(httpCookieStore)
+                    .execute(Request.Get(url));
             
             HttpResponse httpResponse = response.returnResponse();
 
@@ -37,6 +51,14 @@ public class HttpService implements Serializable {
             for (Header header : allHeaders) {
                 System.out.println(header.getName());
                 System.out.println(header.getValue());
+                System.out.println("----------------------------");
+            }
+            
+            System.out.println("------------reading cookie store----------------");
+            List<Cookie> cookies = httpCookieStore.getCookies();
+            for (Cookie cooky : cookies) {
+                String str = String.format("cookie name %s; value %s", cooky.getName(), cooky.getValue());
+                System.out.println(str);
                 System.out.println("----------------------------");
             }
             
