efbceb0055a6bccd55a2852ffa985cc0c1b45b3a
Andrius Ordojan <andrius.or@gmail.com>
2016-01-19 16:00:24 +0100

    Added cookie store

--- src/main/java/org/jenkinsci/plugins/codesonar/services/HttpService.java
+++ src/main/java/org/jenkinsci/plugins/codesonar/services/HttpService.java
@@ -4,14 +4,23 @@ import hudson.AbortException;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.impl.client.BasicCookieStore;
 
 /**
  *
  * @author Andrius
  */
 public class HttpService implements Serializable {
+    CookieStore httpCookieStore;
+    Executor executor;
+    
+    public HttpService() {
+        httpCookieStore = new BasicCookieStore();
+        executor = Executor.newInstance();
+    }
     
     private static final Logger logger = Logger.getLogger(HttpService.class.getName());
     
@@ -19,7 +28,8 @@ public class HttpService implements Serializable {
         logger.fine(String.format("Request sent to %s", url));        
         String output;
         try {
-            output = Request.Get(url).execute().returnContent().asString();
+            output = executor.cookieStore(httpCookieStore)
+                    .execute(Request.Get(url)).returnContent().asString();
         } catch (Exception e) {
             logger.log(Level.SEVERE, String.format("[CodeSonar] Error on url: %s", url), e);
             throw new AbortException(String.format("[CodeSonar] Error on url: %s%n[CodeSonar] Message is: %s", url, e.getMessage()));
