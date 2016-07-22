3463879345c4b7402e2e35ab1e3a5fbc95d4b17f
Andrius Ordojan <andrius.or@gmail.com>
2016-07-22 15:00:54 +0200

    removed checkstyle and added analysis collector.

--- jenkins-pipeline/pipeline.groovy
+++ jenkins-pipeline/pipeline.groovy
@@ -211,7 +211,7 @@ job(ANALYSIS_JOB_NAME) {
 
     steps {
         maven {
-            goals('clean package findbugs:findbugs checkstyle:checkstyle pmd:pmd')
+            goals('clean package findbugs:findbugs pmd:pmd')
             mavenInstallation('Latest')
         }
     }
@@ -220,22 +220,6 @@ job(ANALYSIS_JOB_NAME) {
         buildName('${BUILD_NUMBER}#${GIT_REVISION,length=8}(${GIT_BRANCH})')
     }
     publishers {
-        checkstyle('target/checkstyle-result.xml') {
-            healthLimits(3, 20)
-            thresholdLimit('high')
-            defaultEncoding('UTF-8')
-            canRunOnFailed(true)
-            useStableBuildAsReference(true)
-            useDeltaValues(true)
-            computeNew(true)
-            shouldDetectModules(true)
-            thresholds(
-                    unstableTotal: [all: 1, high: 2, normal: 3, low: 4],
-                    failedTotal: [all: 5, high: 6, normal: 7, low: 8],
-                    unstableNew: [all: 9, high: 10, normal: 11, low: 12],
-                    failedNew: [all: 13, high: 14, normal: 15, low: 16]
-            )
-        }
         findbugs('target/findbugsXml.xml', false) {
             healthLimits(3, 20)
             thresholdLimit('high')
@@ -268,6 +252,11 @@ job(ANALYSIS_JOB_NAME) {
                     unstableNew: [all: 9, high: 10, normal: 11, low: 12],
                     failedNew: [all: 13, high: 14, normal: 15, low: 16]
             )
+        }
+
+        analysisCollector {
+            findbugs()
+            pmd()
         }
 
         downstream(PUSH_TO_JENKINSCI_JOB_NAME, 'SUCCESS')
