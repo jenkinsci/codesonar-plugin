36d3d7a4fddc4e4687c22f217a9ece14b9c09178
Coline <46990780+ColineLelong@users.noreply.github.com>
2019-03-27 16:32:42 +0100

    Update WarningCountIncreaseOverallCondition.java
    Seems that there is an error on "previousBuildAction" usage for further comparison.

--- src/main/java/org/jenkinsci/plugins/codesonar/conditions/WarningCountIncreaseOverallCondition.java
+++ src/main/java/org/jenkinsci/plugins/codesonar/conditions/WarningCountIncreaseOverallCondition.java
@@ -45,7 +45,7 @@ public class WarningCountIncreaseOverallCondition extends Condition {
             return Result.SUCCESS;
         }
 
-        CodeSonarBuildActionDTO prevBuildActionDTO = buildAction.getBuildActionDTO();
+        CodeSonarBuildActionDTO prevBuildActionDTO = previousBuildAction.getBuildActionDTO();
         if (prevBuildActionDTO == null) {
             return Result.SUCCESS;
         }
