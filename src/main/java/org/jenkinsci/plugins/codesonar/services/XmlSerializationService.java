a6ac7a11b50dfb6b0cad742cdad5e89299cdee70
Mads Jensen <mads.jensen@eficode.com>
2020-03-12 16:44:27 +0100

    Fixed #55 -- The class loader should contain path from plugin.
    Co-Authored-By: Mads Nielsen <man@praqma.net>

--- src/main/java/org/jenkinsci/plugins/codesonar/services/XmlSerializationService.java
+++ src/main/java/org/jenkinsci/plugins/codesonar/services/XmlSerializationService.java
@@ -8,8 +8,6 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.Serializable;
 
-
-
 /**
  *
  * @author andrius
@@ -17,14 +15,18 @@ import java.io.Serializable;
 public class XmlSerializationService implements Serializable {
 
     public <T extends Serializable> T deserialize(InputStream content, Class<T> t) throws AbortException {
+        ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
         try {
+            Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
             JAXBContext context = JAXBContext.newInstance(t);
             Unmarshaller un = context.createUnmarshaller();
             BufferedInputStream bis = new BufferedInputStream(content);
-            
+
             return (T) un.unmarshal(bis);
         } catch (JAXBException ex) {
             throw new AbortException(ex.getMessage());
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalClassLoader);
         }
     }
 }
