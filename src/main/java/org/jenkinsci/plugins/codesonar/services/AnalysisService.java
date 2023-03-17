3950d74a55db2e1d352aba4cf4180b023a0df3be
Alessandro Seno <aseno@grammatech.com>
2023-03-17 14:56:56 +0100

    Surrounding parameter 'filter' only for non-numeric values

--- src/main/java/org/jenkinsci/plugins/codesonar/services/AnalysisService.java
+++ src/main/java/org/jenkinsci/plugins/codesonar/services/AnalysisService.java
@@ -113,8 +113,15 @@ public class AnalysisService implements IAnalysisService {
     }
     
     private String formatParameter(String parameter) {
-        if(strictQueryParameters) {
-            return String.format("\"%s\"", parameter);
+        //Remove any unwanted leading or trailing white space character
+        parameter = StringUtils.strip(parameter);
+        try {
+            Long.parseLong(parameter);
+        } catch(NumberFormatException e) {
+            if(strictQueryParameters) {
+                //Surround with double quotes only if parameter is not numeric and the hub supports strict query parameters
+                parameter = String.format("\"%s\"", parameter);
+            }
         }
         return parameter;
     }
@@ -146,4 +153,5 @@ public class AnalysisService implements IAnalysisService {
     public String getVisibilityFilterNewWarningsOrDefault() {
         return StringUtils.isNotBlank(visibilityFilterNewWarnings) ? visibilityFilterNewWarnings : IAnalysisService.VISIBILITY_FILTER_NEW_WARNINGS_DEFAULT;
     }
+    
 }
