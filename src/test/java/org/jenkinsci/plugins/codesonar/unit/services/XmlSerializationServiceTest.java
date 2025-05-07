3306efbc6e1a006ac91abdf1f8c21951fe2939ff
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-05-07 11:35:04 +0200

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/plugins/codesonar/unit/services/XmlSerializationServiceTest.java
+++ src/test/java/org/jenkinsci/plugins/codesonar/unit/services/XmlSerializationServiceTest.java
@@ -1,6 +1,7 @@
 package org.jenkinsci.plugins.codesonar.unit.services;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -9,26 +10,24 @@ import org.jenkinsci.plugins.codesonar.CodeSonarPluginException;
 import org.jenkinsci.plugins.codesonar.models.Metric;
 import org.jenkinsci.plugins.codesonar.models.projects.Project;
 import org.jenkinsci.plugins.codesonar.services.XmlSerializationService;
-import org.junit.Before;
-import org.junit.Test;
-
-import hudson.AbortException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  *
  * @author andrius
  */
-public class XmlSerializationServiceTest {
+class XmlSerializationServiceTest {
 
     private XmlSerializationService xmlSerializationService;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         xmlSerializationService = new XmlSerializationService();
     }
-    
+
     @Test
-    public void providedValidXML_deserializesTheXml() throws CodeSonarPluginException {
+    void providedValidXML_deserializesTheXml() throws CodeSonarPluginException {
         final InputStream VALID_XML_CONTENT = new ByteArrayInputStream((
                 "<project url=\"/analysis/8.xml?filter=2&amp;prj_filter=10\">\n"
                 + "<name>coverity</name>\n"
@@ -36,7 +35,7 @@ public class XmlSerializationServiceTest {
                 + "<started>Fri Feb 13 17:33:18 2015</started>\n"
                 + "<metric name=\"Lines with Code\">0</metric>\n"
                 + "</project>").getBytes());
-        
+
         Project EXPECTED_RESULT = new Project();
         EXPECTED_RESULT.setUrl("/analysis/8.xml?filter=2&prj_filter=10");
         EXPECTED_RESULT.setName("coverity");
@@ -49,16 +48,16 @@ public class XmlSerializationServiceTest {
         assertEquals(EXPECTED_RESULT, result);
     }
 
-    @Test(expected = CodeSonarPluginException.class)
-    public void providedInvalidXML_throwsAbortException() throws CodeSonarPluginException {
+    @Test
+    void providedInvalidXML_throwsAbortException() {
         final InputStream INVALID_XML_CONTENT = new ByteArrayInputStream((
-                "project url=\"/analysis/8.xml?filter=2&prj_filter=10\">\n"
-                + "<name>coverity</name>\n"
-                + "<state>Finished</state>\n"
-                + "started>Fri Feb 13 17:33:18 2015</started>\n"
-                + "<metric name=\"Lines with Code\">0</metric>\n"
-                + "</project>").getBytes());
-
-        xmlSerializationService.deserialize(INVALID_XML_CONTENT, Project.class);
+                    "project url=\"/analysis/8.xml?filter=2&prj_filter=10\">\n"
+                            + "<name>coverity</name>\n"
+                            + "<state>Finished</state>\n"
+                            + "started>Fri Feb 13 17:33:18 2015</started>\n"
+                            + "<metric name=\"Lines with Code\">0</metric>\n"
+                            + "</project>").getBytes());
+        assertThrows(CodeSonarPluginException.class, () ->
+            xmlSerializationService.deserialize(INVALID_XML_CONTENT, Project.class));
     }
 }
